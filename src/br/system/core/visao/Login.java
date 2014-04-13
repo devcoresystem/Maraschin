336ca88f93f2581ff7e8f579e4c99f00cb42412b
Daniel Alves <Daniel Alves@192.168.4.8>
2014-04-13 15:56:32 -0400

    Apagando comentário e dando comit para teste.

--- src/br/system/core/visao/Login.java
+++ src/br/system/core/visao/Login.java
@@ -2,7 +2,6 @@ package br.system.core.visao;
 
 public class Login {
 	public static void main(String [] args){
-		//Visualizando alterações no GitHub
 		javax.swing.JOptionPane.showMessageDialog(null, "Teste");
 	}
 }
